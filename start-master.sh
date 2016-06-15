249c17a08fa94c0d210f4ae922365f5617ca72ed
Samuel von Baussnern <samuel@contovista.com>
2016-06-15 16:17:33 +0200

    asd

--- start-master.sh
+++ start-master.sh
@@ -7,7 +7,7 @@ docker run \
   -p 8080:8080 \
   -p 4040:4040 \
   -p 7077:7077 \
-  --name master \
+  --name sparkMaster \
   samuelsmal/docker-ubuntu-spark:0.1 \
   /usr/bin/supervisord --configuration=/opt/conf/master.conf
 
