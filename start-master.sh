9f32f7c965ff8959da00f5da664b8065d28c0d20
Samuel von Baussnern <samuel@contovista.com>
2016-06-21 15:29:35 +0200

    fixes master creation script

--- start-master.sh
+++ start-master.sh
@@ -7,7 +7,7 @@ docker run \
   -p 38080:8080 \
   -p 34040:4040 \
   -p 37077:7077 \
-  --name sparkMaster \
+  --hostName sparkMaster \
   samuelsmal/docker-ubuntu-spark:0.1 \
   /usr/bin/supervisord --configuration=/opt/conf/master.conf
 
--- tests/pytest.sh
+++ tests/pytest.sh
@@ -5,5 +5,5 @@ spark-submit \
   --master spark://localhost:7077 \
   --executor-memory 2G \
   --total-executor-cores 2 \
-  $SPARK_HOME/examples/target/spark-examples_2.10-1.6.1.jar \
+  $SPARK_HOME/lib/spark-examples-1.6.1-hadoop2.6.0.jar \
   1000
